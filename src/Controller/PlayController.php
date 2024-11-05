d34e8bdf43bb95e910d9234a23d193e636b64254
nesben <nesben34200@gmail.com>
2024-11-05 18:08:17 +0100

    crudPlay FEAT

--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -18,30 +18,30 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 #[Route('api/play')]
 final class PlayController extends AbstractController
 {
-    #[Route('',name:'app_hand_index', methods: ['GET'])]
-    public function index(PlayRepository $handRepository ): JsonResponse
+    #[Route('',name:'app_play_index', methods: ['GET'])]
+    public function index(PlayRepository $playRepository ): JsonResponse
         {
 
-        $hands = $handRepository->findAll();
+        $plays = $playRepository->findAll();
 
         $data = [];
 
-        foreach ($hands as $hand) {
+        foreach ($plays as $play) {
             $data[] = [
-                "id" => $hand->getId(),
-                "turn" =>$hand->getTurn(),
+                "id" => $play->getId(),
+                "turn" =>$play->getTurn(),
                 "card" => [
-                    "id" => $hand->getCard()->getId(),
-                    "name" => $hand->getCard()->getName(),
+                    "id" => $play->getCard()->getId(),
+                    "name" => $play->getCard()->getName(),
                 ],
                 "user" => [
-                    "id" => $hand->getUser()->getId(),
-                    "name" => $hand->getUser()->getEmail(),
+                    "id" => $play->getUser()->getId(),
+                    "name" => $play->getUser()->getEmail(),
                 ],
                 "party" =>[
-                    "id" => $hand->getParty()->getId(),
-                    "code" =>$hand->getParty()->getCode(),
-                    "winner_id" => $hand->getParty()->getWinner()
+                    "id" => $play->getParty()->getId(),
+                    "code" =>$play->getParty()->getCode(),
+                    "winner_id" => $play->getParty()->getWinner()
                 ]
             ];
         }
@@ -50,74 +50,74 @@ final class PlayController extends AbstractController
     }
 
 
-    #[Route('/{id}', name: 'app_hand_show', methods: ['GET'])]  
-    public function show(int $id, PlayRepository $handRepository): JsonResponse
+    #[Route('/{id}', name: 'app_play_show', methods: ['GET'])]  
+    public function show(int $id, PlayRepository $playRepository): JsonResponse
 {
-    $hand = $handRepository->find($id);
+    $play = $playRepository->find($id);
 
-    if (!$hand) {
+    if (!$play) {
         return new JsonResponse(['error' => 'Play not found'], Response::HTTP_NOT_FOUND);
     }
 
     $data = [
-        "id" => $hand->getId(),
-        "turn" => $hand->getTurn(),
+        "id" => $play->getId(),
+        "turn" => $play->getTurn(),
         "card" => [
-            "id" => $hand->getCard()->getId(),
-            "name" => $hand->getCard()->getName(),
+            "id" => $play->getCard()->getId(),
+            "name" => $play->getCard()->getName(),
         ],
         "user" => [
-            "id" => $hand->getUser()->getId(),
-            "name" => $hand->getUser()->getEmail(),
+            "id" => $play->getUser()->getId(),
+            "name" => $play->getUser()->getEmail(),
         ],
         "party" => [
-            "id" => $hand->getParty()->getId(),
-            "code" => $hand->getParty()->getCode(),
-            "winner_id" => $hand->getParty()->getWinner(),
+            "id" => $play->getParty()->getId(),
+            "code" => $play->getParty()->getCode(),
+            "winner_id" => $play->getParty()->getWinner(),
         ]
     ];
 
     return new JsonResponse($data, Response::HTTP_OK);
 }
-#[Route('/new', name: 'app_hand_new', methods: ['GET', 'POST'])]
+#[Route('/new', name: 'app_play_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
 {
     $data = json_decode($request->getContent(), true);
 
-    $hand = new Play();
-    $form = $this->createForm(PlayType::class, $hand);
+    $play = new Play();
+    $form = $this->createForm(PlayType::class, $play);
     $form->submit($data);
 
     if ($form->isSubmitted() && $form->isValid()) {
         // Vérifiez que les entités liées sont bien définies
-        if (!$hand->getUser()) {
+        if (!$play->getUser()) {
             return new JsonResponse(['error' => 'User not found or not set'], Response::HTTP_BAD_REQUEST);
         }
-        if (!$hand->getCard()) {
+        if (!$play->getCard()) {
             return new JsonResponse(['error' => 'Card not found or not set'], Response::HTTP_BAD_REQUEST);
         }
-        if (!$hand->getParty()) {
+        if (!$play->getParty()) {
             return new JsonResponse(['error' => 'Party not found or not set'], Response::HTTP_BAD_REQUEST);
         }
 
-        $entityManager->persist($hand);
+        $entityManager->persist($play);
         $entityManager->flush();
 
         $responseData = [
-            "id" => $hand->getId(),
-            "turn"=> $hand->getTurn(),
+            "id" => $play->getId(),
+            "turn"=> $play->getTurn(),
             "card" => [
-                "id" => $hand->getCard()->getId(),
-                "name" => $hand->getCard()->getName(),
+                "id" => $play->getCard()->getId(),
+                "name" => $play->getCard()->getName(),
             ],
             "user" => [
-                "id" => $hand->getUser()->getId(),
-                "name" => $hand->getUser()->getEmail(),
+                "id" => $play->getUser()->getId(),
+                "name" => $play->getUser()->getEmail(),
             ],
             "party" => [
-                "id" => $hand->getParty()->getId(),
-                "code" => $hand->getParty()->getCode(),
-                "winner_id" => $hand->getParty()->getWinner(),
+                "id" => $play->getParty()->getId(),
+                "code" => $play->getParty()->getCode(),
+                "winner_id" => $play->getParty()->getWinner(),
             ]
         ];
 
@@ -132,32 +132,98 @@ final class PlayController extends AbstractController
 
     return new JsonResponse(['errors' => $errors], Response::HTTP_BAD_REQUEST);
 }
-    #[Route('/{id}/edit', name: 'app_play_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Play $play, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PlayType::class, $play);
-        $form->handleRequest($request);
+#[Route('/put', name: 'app_play_edit', methods: ['PUT'])]
+public function edit(Request $request, PlayRepository $playRepository, EntityManagerInterface $entityManager): JsonResponse
+{
+    $data = json_decode($request->getContent(), true);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_play_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('play/edit.html.twig', [
-            'play' => $play,
-            'form' => $form,
-        ]);
+    // Vérifiez que l'ID est présent dans la requête
+    if (!isset($data['id'])) {
+        return new JsonResponse(['error' => 'ID is required'], Response::HTTP_BAD_REQUEST);
     }
 
-    #[Route('/{id}', name: 'app_play_delete', methods: ['POST'])]
-    public function delete(Request $request, Play $play, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$play->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($play);
-            $entityManager->flush();
-        }
+    $play = $playRepository->find($data['id']);
 
-        return $this->redirectToRoute('app_play_index', [], Response::HTTP_SEE_OTHER);
+    if (!$play) {
+        return new JsonResponse(['error' => 'Play not found'], Response::HTTP_NOT_FOUND);
     }
+
+    // Mettre à jour le champ `turn` si présent dans les données
+    if (isset($data['turn'])) {
+        $play->setTurn($data['turn']);
+    }
+
+    // Vérifiez la présence des IDs des entités liées
+    if (isset($data['user'])) {
+        $user = $entityManager->getRepository(User::class)->find($data['user']);
+        if (!$user) {
+            return new JsonResponse(['error' => 'User not found'], Response::HTTP_BAD_REQUEST);
+        }
+        $play->setUser($user);
+    }
+
+    if (isset($data['card'])) {
+        $card = $entityManager->getRepository(Card::class)->find($data['card']);
+        if (!$card) {
+            return new JsonResponse(['error' => 'Card not found'], Response::HTTP_BAD_REQUEST);
+        }
+        $play->setCard($card);
+    }
+
+    if (isset($data['party'])) {
+        $party = $entityManager->getRepository(Party::class)->find($data['party']);
+        if (!$party) {
+            return new JsonResponse(['error' => 'Party not found'], Response::HTTP_BAD_REQUEST);
+        }
+        $play->setParty($party);
+    }
+
+    // Enregistrer les modifications
+    $entityManager->flush();
+
+    // Préparer la réponse JSON
+    $responseData = [
+        "id" => $play->getId(),
+        "turn" => $play->getTurn(),
+        "card" => [
+            "id" => $play->getCard()->getId(),
+            "name" => $play->getCard()->getName(),
+        ],
+        "user" => [
+            "id" => $play->getUser()->getId(),
+            "name" => $play->getUser()->getEmail(),
+        ],
+        "party" => [
+            "id" => $play->getParty()->getId(),
+            "code" => $play->getParty()->getCode(),
+            "winner_id" => $play->getParty()->getWinner(),
+        ]
+    ];
+
+    return new JsonResponse($responseData, Response::HTTP_OK);
+}
+
+
+#[Route('/delete', name: 'app_play_delete', methods: ['DELETE'])]
+public function delete(Request $request, PlayRepository $playRepository, EntityManagerInterface $entityManager): JsonResponse
+{
+    $data = json_decode($request->getContent(), true);
+
+    // Vérifiez que l'ID est présent dans la requête
+    if (!isset($data['id'])) {
+        return new JsonResponse(['error' => 'ID is required'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $play = $playRepository->find($data['id']);
+
+    if (!$play) {
+        return new JsonResponse(['error' => 'Play not found'], Response::HTTP_NOT_FOUND);
+    }
+
+    // Supprimer l'objet `PLay`
+    $entityManager->remove($play);
+    $entityManager->flush();
+
+    return new JsonResponse(['message' => 'Successfully deleted'], Response::HTTP_OK);
+}
 }
