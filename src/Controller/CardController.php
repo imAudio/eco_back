59d152bcfcdde97b9566dc040240d30d80e3124a
nesben <nesben34200@gmail.com>
2024-11-05 11:12:06 +0100

    fix key

---
+++ src/Controller/CardController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Card;
+use App\Form\CardType;
+use App\Repository\CardRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('api/card')]
+final class CardController extends AbstractController
+{
+    #[Route('', name: 'app_card_index', methods: ['GET'])]
+    public function index(CardRepository $cardRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $cards = $cardRepository->findAll();
+        $data = $serializer->serialize($cards, 'json');
+
+        return new JsonResponse($data, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/new', name: 'app_card_new', methods: ['POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $card = new Card();
+
+        // Utilisation du formulaire pour la validation et le mapping
+        $form = $this->createForm(CardType::class, $card);
+        $form->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($card);
+            $entityManager->flush();
+
+            $jsonData = $serializer->serialize($card, 'json');
+
+            return new JsonResponse($jsonData, Response::HTTP_CREATED, [], true);
+        }
+
+        return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
+    }
+
+    #[Route('/{id}', name: 'app_card_show', methods: ['GET'])]
+    public function show(Card $card, SerializerInterface $serializer): JsonResponse
+    {
+        $data = $serializer->serialize($card, 'json');
+
+        return new JsonResponse($data, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{id}/edit', name: 'app_card_edit', methods: ['PUT'])]
+    public function edit(Request $request, Card $card, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $form = $this->createForm(CardType::class, $card);
+        $form->submit($data, false); // Le deuxième paramètre indique de ne pas effacer les valeurs existantes
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $jsonData = $serializer->serialize($card, 'json');
+
+            return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+        }
+
+        return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
+    }
+
+    #[Route('/{id}', name: 'app_card_delete', methods: ['DELETE'])]
+    public function delete(Card $card, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($card);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Card deleted'], Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ src/Entity/Card.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CardRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CardRepository::class)]
+#[ApiResource]
+class Card
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?int $value = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $capacity = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+
+    private Collection $relateds;
+
+    public function __construct()
+    {
+        $this->relateds = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?string
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(string $capacity): static
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
